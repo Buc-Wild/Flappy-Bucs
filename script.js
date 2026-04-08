4c47c8ad347db5f5e3aab9cbf9dde096dccdb833
AlexSSimpson <alexsimpson2582@gmail.com>
2026-04-08 13:51:06 -0400

    adding the game window and a test box to display something.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "blue";
-ctx.fillRect(50, 50, 100, 100);
+ctx.fillStyle = "red";
+ctx.fillRect(100, 215, 100, 100);
