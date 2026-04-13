02af74a2d60eb391ca3b369c8d33a4886e7233a6
Logan Bachman <lbachman94@gmail.com>
2026-04-13 12:34:37 -0400

    added game loop to script.js

--- script.js
+++ script.js
@@ -3,7 +3,24 @@ const ctx = canvas.getContext("2d");
 
 const buc = new Buc(150, 350);
 
+// set gravity
+const GRAVITY = 0.5;
+
+function gameLoop() {
+    // clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // update physics
+    buc.update(GRAVITY, canvas.height);
+    
+    // draw
+    buc.draw(ctx);
+    
+    // loop game frames
+    requestAnimationFrame(gameLoop);
+}
+
 // Wait for Buc's image to load before drawing
 buc.image.onload = () => {
-    buc.draw(ctx);
+    gameLoop();
 };
