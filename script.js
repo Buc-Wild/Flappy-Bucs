1e1d7ce8962f2d6148835bd9544644ac993bc489
AlexSSimpson <alexsimpson2582@gmail.com>
2026-04-13 19:47:56 -0400

    Added the funcitonality for the pipes

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+const background = new Image();
+background.src = "assets/Back.png";
+
 const buc = new Buc(150, 350);
+//plank variables
+const planks = [];
+let lastSpawnTime = 0;
+const spawnDelay = 1500;
+
 
 // set gravity
 const GRAVITY = 0.5;
 
-function gameLoop() {
-    // clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
+//gameloop function
+function gameLoop(timestamp) 
+{
+    updateGame(timestamp);    
     // update physics
     buc.update(GRAVITY, canvas.height);
-    
-    // draw
-    buc.draw(ctx);
-    
+    // draw the game
+    drawGame()
     // loop game frames
     requestAnimationFrame(gameLoop);
 }
+requestAnimationFrame(gameLoop);
 
-// Wait for Buc's image to load before drawing
-buc.image.onload = () => {
-    gameLoop();
-};
+function drawGame()
+{
+    //clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //sets the background image
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    
+    for(let plank of planks)
+    {
+        plank.draw(ctx, canvas.height);
+    } 
+    buc.draw(ctx);
+}
+
+function spawnPlank()
+{
+    planks.push(new Plank(canvas.width, canvas.height));
+}
+
+
+function updateGame(timestamp) 
+{
+  if (timestamp - lastSpawnTime > spawnDelay) {
+    spawnPlank();
+    lastSpawnTime = timestamp;
+  }
+
+  for (let plank of planks) {
+    plank.update();
+  }
+
+  for (let i = planks.length - 1; i >= 0; i--) {
+    if (planks[i].isOffScreen()) {
+      planks.splice(i, 1);
+    }
+  }
+}
